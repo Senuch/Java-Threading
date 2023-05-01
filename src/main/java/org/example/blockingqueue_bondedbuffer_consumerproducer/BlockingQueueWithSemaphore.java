607ce8b7aa31470d59ad88f502d47f23a6a27dfa
tech-jerry <s.uzairtariq@gmail.com>
2023-05-02 02:22:39 +0500

    CountingSemaphore

--- src/main/java/org/example/blockingqueue_bondedbuffer_consumerproducer/BlockingQueueWithSemaphore.java
+++ src/main/java/org/example/blockingqueue_bondedbuffer_consumerproducer/BlockingQueueWithSemaphore.java
@@ -2,3 +2,35 @@ package org.example.blockingqueue_bondedbuffer_consumerproducer;
 
 public class BlockingQueueWithSemaphore<T> {
 }
+
+class CountingSemaphore {
+    private int usedPermits = 0;
+    private final int maxCount;
+
+    public CountingSemaphore(int count) {
+        this.maxCount = count;
+    }
+
+    public CountingSemaphore(int count, int initialPermits) {
+        this.maxCount = count;
+        this.usedPermits = count - initialPermits;
+    }
+
+    public synchronized void acquire() throws InterruptedException {
+        while (usedPermits == maxCount) {
+            wait();
+        }
+
+        notify();
+        usedPermits++;
+    }
+
+    public synchronized void release() throws InterruptedException {
+        while (usedPermits == 0) {
+            wait();
+        }
+
+        notify();
+        usedPermits--;
+    }
+}
